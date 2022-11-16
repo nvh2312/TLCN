ccb0fb5ee8043459d00752d342e1aeac8aae9929
nvh2312 <mdr3132003@gmail.com>
2022-11-17 05:02:13 +0700

    update feedback after paying success

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -5,7 +5,7 @@ const AppError = require("./../utils/appError");
 const catchAsync = require("./../utils/catchAsync");
 
 exports.setProductUserIds = catchAsync(async (req, res, next) => {
-  console.log("ok")
+  console.log("ok");
   // Allow nested routes
   if (!req.body.product) req.body.product = req.params.productId;
   if (!req.body.user) req.body.user = req.user.id;
@@ -18,22 +18,22 @@ exports.setProductUserIds = catchAsync(async (req, res, next) => {
       )
     );
   }
-  let kt=0;
-  await data.forEach(async (value, index) => {
-    await value.cart.forEach((value, index) => {
-      if (value.product._id == req.body.product){
-        kt=1;
+  let kt = 0;
+  await data.forEach(async (val, index) => {
+    await val.cart.forEach((value, index) => {
+      if (value.product._id == req.body.product && val.status == "Success") {
+        kt = 1;
         return next();
-      } 
+      }
     });
   });
-  if(kt==0)
-  return next(
-    new AppError(
-      "Bạn chưa thể đánh giá lúc này.Vui lòng mua hàng trước khi đánh giá!!!",
-      403
-    )
-  );
+  if (kt == 0)
+    return next(
+      new AppError(
+        "Bạn chưa thể đánh giá lúc này.Vui lòng mua hàng trước khi đánh giá!!!",
+        403
+      )
+    );
 });
 exports.getTableReview = factory.getTable(Review);
 exports.getAllReviews = factory.getAll(Review);
