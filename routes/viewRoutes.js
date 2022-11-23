647a3e1dd85ca14d1f339271e5cc70447f2dd91b
nvh2312 <mdr3132003@gmail.com>
2022-11-23 19:49:46 +0700

    update title page and icon

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -12,40 +12,40 @@ const router = express.Router();
 router.use(authController.isLoggedIn);
 
 router.get("/login", viewController.alreadyLoggedIn, (req, res, next) => {
-  res.status(200).render("login");
+  res.status(200).render("login",{title: "Login"});
 });
-router.get(
-  "/google",
-  passport.authenticate("google", {
-    session: false,
-    scope: ["email", "profile"],
-    accessType: "offline",
-    approvalPrompt: "force"
-  })
-);
-router.get(
-  "/google/callback",
-  passport.authenticate("google", {
-    session: false,
-    failureRedirect: "/login",
-  }),
-  viewController.googleLogin
-);
+// router.get(
+//   "/google",
+//   passport.authenticate("google", {
+//     session: false,
+//     scope: ["email", "profile"],
+//     accessType: "offline",
+//     approvalPrompt: "force"
+//   })
+// );
+// router.get(
+//   "/google/callback",
+//   passport.authenticate("google", {
+//     session: false,
+//     failureRedirect: "/login",
+//   }),
+//   viewController.googleLogin
+// );
 // router.get("/google/callback", passport.authenticate("google"), (req, res) => {
 //   res.redirect("/");
 // });
 router.use(viewController.errorPage);
 router.get("/", (req, res, next) => {
-  res.status(200).render("dashboard");
+  res.status(200).render("dashboard",{title:"Dashboard"});
 });
 router.get("/users", (req, res, next) => {
-  res.status(200).render("user");
+  res.status(200).render("user",{title:"Manage User"});
 });
 router.get("/products", (req, res, next) => {
-  res.status(200).render("product");
+  res.status(200).render("product",{title:"Manage Product"});
 });
 router.get("/orders", (req, res, next) => {
-  res.status(200).render("order");
+  res.status(200).render("order",{title:"Manage Order"});
 });
 router.get("/orders/:id", async (req, res, next) => {
   try {
@@ -60,13 +60,13 @@ router.get("/orders/:id", async (req, res, next) => {
     const date = theDate.toLocaleString();
     data.date = date;
     data.discount = total - data.totalPrice;
-    res.status(200).render("orderDetail", { data });
+    res.status(200).render("orderDetail", { data ,title:"Order Detail"});
   } catch (error) {
     res.status(200).render("404");
   }
 });
 router.get("/imports", (req, res, next) => {
-  res.status(200).render("import");
+  res.status(200).render("import",{title:"Manage Import"});
 });
 router.get("/imports/:id", async (req, res, next) => {
   try {
@@ -75,19 +75,19 @@ router.get("/imports/:id", async (req, res, next) => {
     const theDate = new Date(Date.parse(data.createdAt));
     const date = theDate.toLocaleString();
     data.date = date;
-    res.status(200).render("importDetail", { data });
+    res.status(200).render("importDetail", { data ,title:"Import Detail"});
   } catch (error) {
     res.status(200).render("404");
   }
 });
 router.get("/brands", (req, res, next) => {
-  res.status(200).render("brand");
+  res.status(200).render("brand",{title:"Manage Brand"});
 });
 router.get("/categories", (req, res, next) => {
-  res.status(200).render("category");
+  res.status(200).render("category" ,{title:"Manage Category"});
 });
 router.get("/reviews", (req, res, next) => {
-  res.status(200).render("review");
+  res.status(200).render("review",{title:"Manage Review"});
 });
 
 module.exports = router;
